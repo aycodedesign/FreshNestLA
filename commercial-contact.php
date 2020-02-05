884e6da26cd1130f4b61d037ea0668ec0d9181da
AYDD <aydd@AYDDs-MacBook-Pro.local>
2020-02-05 11:20:20 -0800

    final project

---
+++ commercial-contact.php
@@ -0,0 +1,53 @@
+  <!-- MODALS -->
+
+  <div class="modal fade" id="contactModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog modal-notify modal-warning" role="document">
+      <!--Content-->
+      <div class="modal-content">
+        <!--Header-->
+        <div class="modal-header text-center">
+          <h4 class="modal-title w-100 font-weight-bold py-2 text-green">Contact Us!</h4>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true" class="white-text">&times;</span>
+          </button>
+        </div>
+
+        <!--Body-->
+        <div class="modal-body">
+
+          <div class="md-form mt-3 mb-5">
+            <h6>Email</h6>
+            <a href="mailto:freshnestla@gmail.com" target="_blank">
+              <p class="mb-4">FreshnestLA@gmail.com</p>
+            </a>
+
+            <h6>Phone</h6>
+            <a href="tel:3238984652">
+              <p class="mb-4">323.898.4652</p>
+            </a>
+          </div>
+
+          <div class="list-inline text-center">
+            <div class="container social-link">
+              <a href="https://www.yelp.com/biz/fresh-nest-los-angeles-2?start=20" target="_blank"
+                class="btn btn-social text-white mr-4"><i class="fab fa-yelp"></i></a>
+              <a href="https://www.instagram.com/freshnestla/?hl=en" target="_blank"
+                class="btn btn-social text-white mr-4"><i class="icon-social-instagram"></i>
+              </a>
+            </div>
+          </div>
+
+        </div>
+
+        <!--Footer-->
+        <div class="modal-footer justify-content-center">
+          <a href="./commercial-cleaning.php#quote" class="btn btn-blue js-scroll-trigger">REQUEST A QUOTE</a>
+        </div>
+
+      </div>
+      <!--/.Content-->
+    </div>
+  </div>
+
+  <!-- /MODALS -->
---
+++ home-quote.php
@@ -0,0 +1,140 @@
+<!-- QUOTE -->
+  <section class="content-section bg-light" id="quote">
+    <div class="container">
+      <h6 class="text-center mb-1">HOME CLEANING</h6>
+      <h2 class="text-center mb-5 text-green">REQUEST A QUOTE</h2>
+      <div class="row">
+        <div class="col-lg-4">
+
+          <form action="/html/tags/html_form_tag_action.cfm">
+
+            <fieldset class="form-group" aria-labelledby="pseudolegend">
+              <h4 class="mb-3">SERVICE</h4>
+
+              <div class="row">
+                <div class="col-9">
+                  <div class="form-check mb-2">
+                    <label class="form-check-label">
+                      <input class="form-check-input" type="checkbox" name="legendCheckbox" id="legendRadio1" value="1">
+                      Weekly
+                    </label>
+                  </div>
+                  <div class="form-check mb-2">
+                    <label class="form-check-label">
+                      <input class="form-check-input" type="checkbox" name="legendCheckbox" id="legendRadio2" value="2">
+                      Bi-weekly
+                    </label>
+                  </div>
+                  <div class="form-check mb-2">
+                    <label class="form-check-label">
+                      <input class="form-check-input" type="checkbox" name="legendCheckbox" id="legendRadio3" value="3">
+                      Deep Cleaning
+                    </label>
+                  </div>
+                  <div class="form-check mb-2">
+                    <label class="form-check-label">
+                      <input class="form-check-input" type="checkbox" name="legendCheckbox" id="legendRadio3" value="4">
+                      Move-in/Move-out
+                    </label>
+                  </div>
+                  <div class="form-check mb-2">
+                    <label class="form-check-label">
+                      <input class="form-check-input" type="checkbox" name="legendCheckbox" id="legendRadio3" value="5">
+                      Airbnb/short-term rental
+                    </label>
+                  </div>
+                  <div class="form-check mb-2">
+                    <label class="form-check-label">
+                      <input class="form-check-input" type="checkbox" name="legendCheckbox" id="legendRadio3" value="5">
+                      Post-construction
+                    </label>
+                  </div>
+                </div>
+              </div>
+            </fieldset>
+
+            <fieldset class="form-group" aria-labelledby="pseudolegend2">
+              <h4 class="mt-4 mb-3">STRUCTURE</h4>
+
+              <div class="row">
+                <div class="col-9">
+                  <div class="form-check mb-2">
+                    <label class="form-check-label">
+                      <input class="form-check-input" type="radio" name="legendRadio" id="legendRadio1" value="1">
+                      House
+                    </label>
+                  </div>
+                  <div class="form-check mb-2">
+                    <label class="form-check-label">
+                      <input class="form-check-input" type="radio" name="legendRadio" id="legendRadio2" value="2">
+                      Apartment/Condominium
+                    </label>
+                  </div>
+                </div>
+              </div>
+            </fieldset>
+
+          </form>
+        </div>
+
+        <div class="col-lg-8 mx-auto">
+          <h4 class="mb-3">CONTACT INFORMATION</h4>
+          <div class="form-group row mb-4">
+            <div class="col-lg-6 col-sm-12 mb-4">
+              <input type="text" class="form-control" id="first_name" name="first_name" placeholder="First Name">
+            </div>
+
+            <div class="col-lg-6 col-sm-12">
+              <input type="text" class="form-control" id="last_name" name="last_name" placeholder="Last Name">
+            </div>
+          </div>
+
+          <div class="form-group row mb-4">
+            <div class="col-lg-6 col-sm-12 mb-4">
+              <input type="text" class="form-control" id="email" name="email" placeholder="Email">
+            </div>
+
+            <div class="col-lg-6 col-sm-12">
+              <input type="text" class="form-control" id="phone" name="phone" placeholder="Phone">
+            </div>
+          </div>
+
+          <div class="form-group row mb-4">
+            <div class="col-lg-6 col-sm-12 mb-4">
+              <input type="text" class="form-control" id="number_bedrooms" name="number_bedrooms"
+                placeholder="Number of Bedrooms">
+            </div>
+
+            <div class="col-lg-6 col-sm-12">
+              <input type="text" class="form-control" id="number_bathrooms" name="number_bathrooms"
+                placeholder="Number of Bathrooms">
+            </div>
+          </div>
+
+          <div class="form-group row mb-4">
+            <div class="col-lg-6 col-sm-12 mb-4">
+              <input type="text" class="form-control" id="square_footage" name="square_footage"
+                placeholder="Square Footage">
+            </div>
+
+            <div class="col-lg-6 col-sm-12 mb-4">
+              <input type="text" class="form-control" id="zip_code" name="zip_code" placeholder="Zip Code">
+            </div>
+          </div>
+
+          <div class="form-group row mb-4">
+            <div class="col-lg-6 col-sm-12 mb-4">
+              <input type="text" class="form-control" id="number_levels" name="number_levels"
+                placeholder="Number of Levels">
+            </div>
+          </div>
+
+          <div class="form-group row mb-5">
+            <div class="col">
+              <button type="submit" class="btn btn-green">Submit</button>
+            </div>
+          </div>
+        </div> <!-- end input column -->
+      </div>
+    </div>
+  </section>
